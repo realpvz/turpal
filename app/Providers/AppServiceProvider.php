cc93685546b457c8625ca5632db27dd624cc1101
norman <hellopvz@gmail.com>
2024-10-10 14:40:37 +0330

    feat(Intialize): binding providers contract with concrete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Contracts\Repositories\ProviderRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ProviderRepository::class, \App\Repositories\Eloquent\ProviderRepository::class);
     }
 
     /**
